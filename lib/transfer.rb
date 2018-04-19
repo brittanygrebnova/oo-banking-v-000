a7c18d6426b5e37956ce18348201892943ec8cba
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-19 12:52:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+  
+  require 'pry'
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+  
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    Transfer.new(sender, receiver, amount)
+    if valid?
+      @status = 'complete'
+    end
+  end
+  
 end
